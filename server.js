9eed74e3f69e3267a352c2b20df48e2f84714b29
Nurul Huda Robin <robin@dexsa.org>
2014-11-23 01:09:00 +0600

    --

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
     app = express();
 
 app
-    .use(express.static('./public'))git
+    .use(express.static('./public'))
     .get('*',function(req,res){
         res.sendfile('public/main.html');
     })
